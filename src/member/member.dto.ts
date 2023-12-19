544ad06f6d10d25d93f65a2ec041d70ce2079bba
shimjaedoo <woen1637@naver.com>
2023-12-19 23:55:08 +0900

    feat: update DTO 추가

--- src/member/member.dto.ts
+++ src/member/member.dto.ts
@@ -47,3 +47,44 @@ export class CreateMemberDto {
   @IsNotEmpty()
   role: boolean;
 }
+
+export class UpdateMemberDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(4)
+  @MinLength(2)
+  @Matches(/[가-힣]/)
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(10)
+  @MinLength(2)
+  @Matches(/^[ㄱ-ㅎ가-힣a-zA-Z0-9]+$/)
+  nickname: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(16)
+  @MinLength(8)
+  @Matches(/^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])/)
+  password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(13)
+  @Matches(/^01(?:0|1|[6-9])-(?:\d{3}|\d{4})-\d{4}$/)
+  tel: string;
+
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+
+  @IsString()
+  @IsNotEmpty()
+  subAddress: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  role: boolean;
+}
