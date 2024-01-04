a001ab57efd81a3fddce5f1a4f117acda0ca130c
shimjaedoo <woen1637@naver.com>
2024-01-04 11:22:04 +0900

    fix: Price number변경

--- src/product/product.dto.ts
+++ src/product/product.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateProductDto {
   @IsNotEmpty({ message: '상품의 이름을 입력해주세요.' })
@@ -7,12 +7,14 @@ export class CreateProductDto {
   name: string;
 
   @IsNotEmpty({ message: '상품의 가격을 입력해주세요.' })
-  @IsString()
-  price: string;
+  @IsNumber()
+  price: number;
 
   @IsNotEmpty({ message: '상품을 설명해주세요.' })
   @IsString()
   @MaxLength(100)
-  @MinLength(10)
+  @MinLength(2)
   desc: string;
+
+  salesId: string;
 }
